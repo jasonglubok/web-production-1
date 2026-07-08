8512eeacecfcd6d156011812ba6b1866038975c9
jasonglubok <jason.glubok80@myhunter.cuny.edu>
2026-07-08 14:09:24 -0400

    Add files via upload

---
+++ class-demos/class_12/script.js
@@ -0,0 +1,23 @@
+// What is the first line of code we write in a .js file?
+// Write it below this line.
+window.addEventListener("load",()=>{
+	let container = document.querySelector("#container")
+container.addEventListener("mouseenter",()=>{
+	container.style.backgroundColor = "#c0d6df"
+})
+
+container.addEventListener()
+	container.style.backgroundColor = "#e63946"
+
+
+
+// Inside of the function, retrieve the id of the div you created in index.html and assign it to a variable.
+
+// Q: What is another way to write the first line of code? 
+// A: Window.onload = () => {}
+
+// Q: What does the document keyword represent?
+// A: the html document or the DOM
+
+// Q: What is the property we access to change the style of a selected element in js? How would we change the background color of the div we selected?
+// A: document.querySelector("#container")/style.backgroundColor
